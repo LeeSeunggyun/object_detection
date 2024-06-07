6637749d8c3abd19b7a483522a42dca9c779b21a
LSG <2021427992lsg@gmail.com>
2024-06-07 12:53:02 +0900

    modularize detection/additional test

--- object_detect.py
+++ object_detect.py
@@ -8,6 +8,25 @@ import torch
 import shutil
 
 
+
+# model_test/src 폴더에 있는 모든 이미지들을 models 폴더의 모든 모델로 검사
+# detect() 
+
+
+# 이미지 파일 하나를 모든 모델로 검사
+# detect_with_image("model_test/src/KakaoTalk_20240602_225259258.jpg")
+# detect_with_image("model_test/src/KakaoTalk_20240602_225259258_01.jpg")
+# detect_with_image("model_test/src/KakaoTalk_20240602_230702558.jpg")
+# detect_with_image("model_test/src/KakaoTalk_20240602_230702558_01.jpg")
+
+
+# 이미지 파일 하나를 하나의 모델로 검사
+# detect_indiviaully(input_image_path = "model_test/src/KakaoTalk_20240602_225259258.jpg", 
+#                 model_output_folder = "model_test/individual_test", 
+#                 model_path = "models/best137.pt")
+
+
+
 def image_detection(image_path, save_path, model, classNames, targets):
     #--------------------------------추론----------------------------------#
     # 이미지 불러오기
@@ -59,39 +78,56 @@ def image_detection(image_path, save_path, model, classNames, targets):
     return result_image_path
 
 
+def detect():
+    # 모델들 있는 경로
+    model_path = "./models"
+    # 검사할 이미지 있는 경로
+    input_folder = "./model_test/src"
+    # 검사 결과들 저장할 경로
+    output_folder = "./model_test"
 
+    # Create a directory for each model
+    for model_name in os.listdir(model_path):
+        model_output_folder = os.path.join(output_folder, model_name)
+        if not os.path.exists(model_output_folder):
+            os.makedirs(model_output_folder)
 
+        print(model_name)
+        if model_name in ["best137.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28']
+            targets=['2','4','6','8','10','12']
+        elif model_name in ["best232.pt", "best232_3080.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']
+            targets=['0']
+        elif model_name in ["best307.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8']
+            targets=['3']
+        elif model_name in ["best232_reducedClass.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8','9']
+            targets=['0']
+        else:
+            print("Invalid model name")
+            # Remove the model output folder
+            shutil.rmtree(model_output_folder)
+            continue
+            
+        model = YOLO(os.path.join(model_path, model_name))
+        for filename in os.listdir(input_folder):
+            if filename.endswith(".jpg"):
+                input_image_path = os.path.join(input_folder, filename)
+                # 추론 수행 + 결과 저장
+                result_image_path = image_detection(input_image_path, 
+                                                    model_output_folder,
+                                                    model,
+                                                    classNames,
+                                                    targets)
 
-# 모델 불러오기
-# model_path="models/best232_3080.pt"
+def detect_indiviaully(input_image_path, model_output_folder, model_path):
 
-# if model_path == "models/best137.pt":
-#     classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28']
-#     targets=['2','4','6','8','10','12']
-# elif model_path == "models/best232.pt" or model_path == "models/best232_3080.pt":
-#     classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']
-#     targets=['0']
-# elif model_path == "models/best307.pt":
-#     classNames = ['0','1','2','3','4','5','6','7','8']
-#     targets=['3']
-# elif model_path == "models/best232_again.pt":
-#     classNames = ['0','1','2','3','4','5','6','7','8','9']
-#     targets=['0']
+    # get model name if model_name is from of path
+    if model_path.find("/") != -1:
+        model_name = model_path.split("/")[-1]
 
-# 모델들 있는 경로
-model_path = "./models"
-# 검사할 이미지 있는 경로
-input_folder = "./model_test/src"
-# 검사 결과들 저장할 경로
-output_folder = "./model_test"
-
-# Create a directory for each model
-for model_name in os.listdir(model_path):
-    model_output_folder = os.path.join(output_folder, model_name)
-    if not os.path.exists(model_output_folder):
-        os.makedirs(model_output_folder)
-
-    print(model_name)
     if model_name in ["best137.pt"]:
         classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28']
         targets=['2','4','6','8','10','12']
@@ -106,17 +142,49 @@ for model_name in os.listdir(model_path):
         targets=['0']
     else:
         print("Invalid model name")
-        # Remove the model output folder
-        shutil.rmtree(model_output_folder)
-        continue
         
-    model = YOLO(os.path.join(model_path, model_name))
-    for filename in os.listdir(input_folder):
-        if filename.endswith(".jpg"):
-            input_image_path = os.path.join(input_folder, filename)
-            # 추론 수행 + 결과 저장
-            result_image_path = image_detection(input_image_path, 
-                                                model_output_folder,
-                                                model,
-                                                classNames,
-                                                targets)
+    model = YOLO(model_name)
+    result_image_path = image_detection(input_image_path, 
+                                        model_output_folder,
+                                        model,
+                                        classNames,
+                                        targets)
+
+# image detection with each models with image file only
+def detect_with_image(input_image_path):
+    # 모델들 있는 경로
+    model_path = "./models"
+    # 검사 결과들 저장할 경로
+    output_folder = "./model_test"
+
+    # Create a directory for each model
+    for model_name in os.listdir(model_path):
+        model_output_folder = os.path.join(output_folder, model_name)
+        if not os.path.exists(model_output_folder):
+            os.makedirs(model_output_folder)
+
+        print(model_name)
+        if model_name in ["best137.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28']
+            targets=['2','4','6','8','10','12']
+        elif model_name in ["best232.pt", "best232_3080.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']
+            targets=['0']
+        elif model_name in ["best307.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8']
+            targets=['3']
+        elif model_name in ["best232_reducedClass.pt"]:
+            classNames = ['0','1','2','3','4','5','6','7','8','9']
+            targets=['0']
+        else:
+            print("Invalid model name")
+            continue
+            
+        model = YOLO(os.path.join(model_path, model_name))
+        # 추론 수행 + 결과 저장
+        result_image_path = image_detection(input_image_path, 
+                                            model_output_folder,
+                                            model,
+                                            classNames,
+                                            targets)
+
